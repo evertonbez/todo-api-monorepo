b9aa30070ef481ce50d8308e35d03dd78ae7a988
Everton <evertonfrnds@gmail.com>
2025-08-25 13:50:14 -0300

    add unique

--- todo-backend/src/main/java/com/evertonbez/todo_app/entities/Todo.java
+++ todo-backend/src/main/java/com/evertonbez/todo_app/entities/Todo.java
@@ -1,5 +1,6 @@
 package com.evertonbez.todo_app.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,7 +22,10 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String name;
+
     private BigDecimal price;
     private LocalDate limitDate;
     private Integer reOrder;
